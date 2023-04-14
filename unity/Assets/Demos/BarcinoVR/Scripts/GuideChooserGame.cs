c7734f8f64e3f3ef6afd3240ef1d93d716ecd9b7
Myriam Chamoun <97394952+mgchamoun@users.noreply.github.com>
2023-04-14 13:24:07 +0200

    Fixing waypoints

--- unity/Assets/Demos/BarcinoVR/Scripts/GuideChooserGame.cs
+++ unity/Assets/Demos/BarcinoVR/Scripts/GuideChooserGame.cs
@@ -6,14 +6,33 @@ namespace WorldAsSupport
 {
     public class GuideChooserGame : InteractableGame
     {
+        
         protected override void ChildStart()
         {
+            ARGameSession.current.WaypointProvider.enabled = false;
+
         }
         protected override void Grabbed(InteractableItem guide)
         {
             Debug.Log("Grabbed " + guide.name);
+            ARGameSession.current.WaypointProvider.enabled = true;
             ARGameSession.current.WaypointProvider.GuidePrefabs[0] = guide.GetComponent<WaypointGuide>();
+            
+            HumanGuide hg = guide.GetComponent<HumanGuide>();
+            if (hg != null){
+                hg.enabled = true;
+            }
 
+            ArrowGuide ag = guide.GetComponent<ArrowGuide>();
+            if (ag != null){
+                ag.enabled = true;
+            }
+
+            WaypointGuide wg = guide.GetComponent<WaypointGuide>();
+            if (wg != null && guide.gameObject.tag != "SparkleGuide"){
+                wg.enabled = true;
+            }
+            
             // if (guide.name == "Arrow Guide" || guide.name == "Sparkles Guide" || guide.name == "Human Guide")
             //  {
             //     WaypointProvider waypointProvider = FindObjectOfType<WaypointProvider>();
